d960fd91c132879dccfb29f474e47933e3f2bd74
Parishkar Singh <79470399+parishkar-9790@users.noreply.github.com>
2023-03-05 00:59:19 +0530

    feat: Added login and register pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import './app.scss';
 import Home from './pages/home/Home';
 import Watch from './pages/watch/Watch';
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
 const App = ()=> {
   return (
-    <Watch/>
+    // <Register/>
+      // <Watch/>
+      <Login/>
       // <Home />)
   );
 }
