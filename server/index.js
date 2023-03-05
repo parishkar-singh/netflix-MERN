c6723efea2179eb6c72435fbb3ddeb37160594cc
Parishkar Singh <79470399+parishkar-9790@users.noreply.github.com>
2023-03-05 11:23:17 +0530

    feat: routes added

--- server/index.js
+++ server/index.js
@@ -3,11 +3,21 @@ const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
+const authRoute=require("./routes/auth")
+
+//middleware
+app.use("/api/auth",authRoute);
+app.use(express.json())
+
+// Database
 mongoose.connect(process.env.DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    // useCreateIndex:true
 }).then(()=>console.log("Database Connected! Lets go"))
 PORT = process.env.PORT || 9999
+
+//Express server
 app.listen(PORT, () => {
     console.log("Express server Up and running @", PORT)
 })
