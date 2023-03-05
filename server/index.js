8de7b2cab62e57fb08a8ef31780eae25df0d0b0f
Parishkar Singh <79470399+parishkar-9790@users.noreply.github.com>
2023-03-05 10:29:00 +0530

    Feat: Done User, Movie, List schema's

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
-const express =require("express")
-const app=express()
-const mongoose=require('mongoose')
-
-app.listen(process.env.PORT,()=>{
-    console.log("Backend server Up and running")
+const express = require("express")
+const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+mongoose.connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=>console.log("Database Connected! Lets go"))
+PORT = process.env.PORT || 9999
+app.listen(PORT, () => {
+    console.log("Express server Up and running @", PORT)
 })
